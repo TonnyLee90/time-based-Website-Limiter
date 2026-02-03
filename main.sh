91aa2c820da5bd6ebcce73c834a6c93792d69881
tonny <tonny.lee@sjsu.edu>
2026-02-03 09:00:39 +0000

    timer added. when the time is up, the url will be avaliable

--- main.sh
+++ main.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 while [[ true ]]
@@ -5,10 +6,18 @@ do
 	read -p "Enter an URL: " url
 	read -p "You entered: $url, correct? [Y/N] " response
 	if [[ $response == "Y" || $response == "y" ]]; then
-        	echo "$url is blocked"
 		echo "127.0.0.1 $url" >> blocked_urls.txt
 		break
 	else
         	echo "Please, enter again."
 	fi
 done
+
+#timer
+read -p "How long should the hosts be blocked (in seconds)? " time
+echo "The hosts will be avaliable after $time second(s)"
+echo "$url is blocked"
+# when timeed out, remove the url from blocked_urls
+sleep $time
+sed -i "/127.0.0.1 $url/d" blocked_urls.txt
+echo "$url is removed"
