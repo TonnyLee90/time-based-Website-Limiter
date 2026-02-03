5ee265ebb8f2def814585e1fe545893e537a1b34
tonny <tonny.lee@sjsu.edu>
2026-02-03 07:07:15 +0000

    All blocked urls will be shown in the blocked_urls.txt

--- main.sh
+++ main.sh
@@ -6,6 +6,7 @@ do
 	read -p "You entered: $url, correct? [Y/N] " response
 	if [[ $response == "Y" || $response == "y" ]]; then
         	echo "$url is blocked"
+		echo "127.0.0.1 $url" >> blocked_urls.txt
 		break
 	else
         	echo "Please, enter again."
